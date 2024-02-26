ea9a26b373361bd244ef954aef956a62f6411791
Youri Schijff <yourischijff@gmail.com>
2024-02-26 11:30:31 +0100

    Assertions

--- Lila/src/Lila/core/Logger.h
+++ Lila/src/Lila/core/Logger.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <iostream>
-
+#include <cstring>
 
 namespace Lila {
 	
 	typedef enum LogLevel {
 		LL_FATAL,
+		LL_ASSERT,
 		LL_ERROR,
 		LL_WARN,
 		LL_DEBUG,
@@ -15,18 +16,19 @@ namespace Lila {
 	};
 
 	static int lastLogLevel = LL_WARN;
-	
+
 	static void _log(LogLevel logLevel, const char* message, ...) {
 		if(lastLogLevel < logLevel)
 			return;
 		//#ifdef _WIN32
 		static char* textColor[] = {
-			"\x1b[31m", // Dark Red (LL_FATAL)
-			"\x1b[91m", // Red      (LL_ERROR)
-			"\x1b[93m", // Yellow   (LL_WARN)
-			"\x1b[96m", // Cyan     (LL_DEBUG)
-			"\x1b[94m", // Blue     (LL_INFO)
-			"\x1b[35m"  // Purple   (LL_TRACE)
+			"\x1b[31m",        // Dark Red (LL_FATAL)
+			"\x1b[1m\x1b[91m", // Bold Red (LL_ASSERT)
+			"\x1b[91m",        // Red      (LL_ERROR)
+			"\x1b[93m",        // Yellow   (LL_WARN)
+			"\x1b[96m",        // Cyan     (LL_DEBUG)
+			"\x1b[94m",        // Blue     (LL_INFO)
+			"\x1b[35m"         // Purple   (LL_TRACE)
 		};
 
 		char* logColor = textColor[logLevel];
@@ -34,6 +36,7 @@ namespace Lila {
 
 		static char* prefixes[] = {
 			"[FATAL] : ",
+			"[ASSERT]: ",
 			"[ERROR] : ",
 			"[WARN]  : ",
 			"[DEBUG] : ",
@@ -46,12 +49,18 @@ namespace Lila {
 
 		char out[1024];
 		memset(out, 0, sizeof(out));
-
+	
+	#ifdef _WIN32
 		va_list arg_ptr;
 		__crt_va_start(arg_ptr, message);
 		vsprintf(out, message, arg_ptr);
 		__crt_va_end(arg_ptr);
-
+	#else
+		va_list arg_ptr;
+		__builtin_va_start(arg_ptr, message);
+		vsprintf(out, message, arg_ptr);
+		__builtin_va_end(arg_ptr);
+	#endif
 		printf("%s%s", logColor, prefix);
 		printf(out);
 		printf("%s\n", endColor);
@@ -66,10 +75,25 @@ namespace Lila {
 
 #ifdef _WIN32
 #define DEBUG_BREAK() __debugbreak();
-#elif defined(__linux__)
-#define DEBUG_BREAK() __builtin_debugtrap();
-#elif defined(__APPLE__)
+#else
+
 #define DEBUG_BREAK() __builtin_trap();
 #endif
+
+#define assertL(expr) {			\
+		if(expr) {\
+		} else {\
+			_log(Lila::LL_ASSERT,"Assertion: %s, in file %s:%d", #expr, __FILE__, __LINE__);\
+			DEBUG_BREAK();\
+		}\
+	}
+
+#define assertM(expr, message) {			\
+		if(expr) {\
+		} else {\
+			_log(Lila::LL_ASSERT,"Assertion: %s, message: %s, in file %s:%d", #expr, message, __FILE__, __LINE__);\
+			DEBUG_BREAK();\
+		}\
+	}
 
 }
