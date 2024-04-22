147bd4a05f7d9838272a6b29389c91be72556f27
Youri Schijff <yourischijff@gmail.com>
2024-04-22 23:20:09 +0200

    Added basic math

--- Lila/src/Lila/Lila.h
+++ Lila/src/Lila/Lila.h
@@ -4,6 +4,9 @@
 #include "Lila/utils/Memory.h"
 #include "Lila/utils/UUID.h"
 
+#include "Lila/math/Math.h"
+#include "Lila/math/Graph.h"
+
 #include "Lila/core/Application.h"
 #include "Lila/core/Logger.h"
 
---
+++ Lila/src/Lila/math/Vectors.h
@@ -0,0 +1,52 @@
+#pragma once
+
+namespace Lila {
+	
+	class Vec2 {
+	public:
+		Vec2(float x, float y) { 
+			val[0] = x;
+			val[1] = y;
+		}
+
+		float x() { return val[0]; }
+		float y() { return val[1]; }
+
+	private:
+		float val[2] { 0.0f, 0.0f };
+	};
+
+	class Vec3 {
+	public:
+		Vec3(float x, float y, float z) {
+			val[0] = x;
+			val[1] = y;
+			val[2] = z;
+		}
+
+		float x() { return val[0]; }
+		float y() { return val[1]; }
+		float z() { return val[2]; }
+
+	private:
+		float val[3]{ 0.0f, 0.0f, 0.0f };
+	};
+
+	class Vec4 {
+	public:
+		Vec4(float x, float y, float z, float w) {
+			val[0] = x;
+			val[1] = y;
+			val[2] = z;
+			val[3] = w;
+		}
+
+		float x() { return val[0]; }
+		float y() { return val[1]; }
+		float z() { return val[2]; }
+		float w() { return val[3]; }
+
+	private:
+		float val[4]{ 0.0f, 0.0f, 0.0f, 0.0f };
+	};
+}
