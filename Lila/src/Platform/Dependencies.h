4cda7a9699765bfb4aa803eba7c06233e11d1671
Youri Schijff <yourischijff@gmail.com>
2024-04-23 19:47:47 +0200

    Auto-init some dependencies

---
+++ Lila/src/Platform/Dependencies.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Lila/core/Logger.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+static bool initDependencies() {
+	if(!glfwInit()) {
+		lila_fatal("Failed to initalize GLFW!")
+		return false;
+	}
+
+	lila_trace("Initialized GLFW");
+	lila_info("GLFW version %d.%d.%d", GLFW_VERSION_MAJOR, GLFW_VERSION_MINOR, GLFW_VERSION_REVISION);
+}
+
+static bool initOpenGL() {
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		lila_fatal("Failed to initalize OpenGL!")
+			return false;
+	}
+
+	lila_trace("Initialized OpenGL");
+	lila_info("OpenGL version %s", glGetString(GL_VERSION));
+}
