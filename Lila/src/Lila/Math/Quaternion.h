138ac851fb413cb87bbfb4fe7288cd0993aebf85
Youri Schijff <yourischijff@gmail.com>
2024-06-03 15:53:41 +0200

    Added Quaternions

--- Lila/src/Lila/Math/Quaternion.h
+++ Lila/src/Lila/Math/Quaternion.h
@@ -2,18 +2,51 @@
 
 #include "Platform/Defines.h"
 
+#include "Vectors.h"
+
 namespace Lila {
 
 	class Quaternion {
 	public:
-		Quaternion(f32 a, f32 b, f32 c, f32 d) {
-			val_m[0] = a;
-			val_m[1] = b;
-			val_m[2] = c;
-			val_m[3] = d;
-		}
+		Quaternion();
+		Quaternion(Vec4& vec);
+		Quaternion(f32 h, f32 i, f32 j, f32 k);
+
+		Quaternion(Vec3& euler);
+		Quaternion(Vec3& axis, float angle);
+
+		~Quaternion();
+
+		// Returns a standard quaternion.
+		static Quaternion identity();
+
+		// Creates a quaternion from an euler rotation.
+		static Quaternion euler(Vec3& euler);
+
+		Quaternion normalize();
+		Quaternion normalize(Quaternion& q);
+
+		Quaternion inverse();
+		Quaternion inverse(Quaternion* q);
+
+		Quaternion conjugate();
+
+		f32 magnitude();
+
+		Quaternion operator*(f32 scale);
+		Quaternion operator*(Quaternion& q);
+
+		void operator*=(f32 scale);
+		void operator*=(Quaternion& q);
+
+		Quaternion operator+(Quaternion& q);
+		Quaternion operator-(Quaternion& q);
+
+		Quaternion copy();
 
 	private:
-		f32 val_m[4]{ 0.0f, 0.0f, 0.0f, 0.0f };
+		// A Quaternion is defined by 1 real number (h = 1) and 3 imaginary numbers (i = j = k = 0).
+		f32 h,i,j,k;
 	};
-}
+
+} // namespace Lila
