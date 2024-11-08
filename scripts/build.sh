737c42280f48a45c20a5570b712232002f937ce1
Youri Schijff <yourischijff@gmail.com>
2024-11-08 11:39:49 +0100

    Added scripts

---
+++ scripts/build.sh
@@ -0,0 +1,28 @@
+if [[ ! -d "Lila" ]]; then
+    echo You must run the script in the root folder!
+    exit -1
+fi
+
+cd Lila
+if [[ ! -d "src" ]]; then
+    cd ../
+    echo You must run the script in the root folder!
+    exit -1
+fi
+cd ../
+
+BUILD_DIR="buildLinux"
+
+cmake -S . -B $BUILD_DIR -D GLFW_BUILD_WAYLAND=OFF
+
+echo ====================
+echo Lila setup finished!
+
+cd $BUILD_DIR
+
+make
+
+echo ====================
+echo Lila build finished!
+
+"./LilaEngine"
