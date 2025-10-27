35008ab046e205f1151236fd774754b452a5fb2e
Youri <yourischijff@gmail.com>
2025-10-27 10:51:28 +0100

    Updated Main.cpp
    Removed all the Lila Math functionality.
    Setup a simple cube and camera example.
    Fixed the vertices and indices to render a cube

--- App/assets/default.vert
+++ App/assets/default.vert
@@ -1,11 +1,14 @@
 #version 330 core
 
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 texcoords;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 texcoords;
+
+uniform mat4 view;
+uniform mat4 projection;
 
 out vec3 pos;
 
 void main() {
-    gl_Position = vec4(position, 1.0);
+    gl_Position = projection * view * vec4(position, 1.0);
     pos = position;
 }
--- App/src/Main.cpp
+++ App/src/Main.cpp
@@ -1,3 +1,6 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
 #include "Renderer/OpenGL/GLGeometry.h"
 #include "Renderer/OpenGL/GLShader.h"
 
@@ -12,14 +15,11 @@
 #include "ECS/Components.h"
 #include "ECS/ComponentManager.h"
 
-#include "Math/Matrix.h"
-#include "Math/Quaternion.h"
-#include "Math/Vec2.h"
-#include "Math/Vec3.h"
-
 #include "Event/EventBus.h"
 
 #include "Input/Keyboard.h"
+
+#include "World/Camera.h"
 
 struct Transform {
     f32 x, y, z;
@@ -29,9 +29,26 @@ struct Tag {
     u64 tag;
 };
 
+f32 camX = 0, camY = 0, camZ = 0;
+f32 camSpeed = 0.05;
+
 void keyEventFunction(Lila::KeyEvent event) {
-    LILA_DEBUG("Key {}, Action {}", event.key, event.action);
+    if(event.key == 65)
+        camZ -= camSpeed;
+    if(event.key == 68)
+        camZ += camSpeed;
+
+    if(event.key == 340)
+        camY -= camSpeed;
+    if(event.key == 32)
+        camY += camSpeed;
+
+    if(event.key == 87)
+        camX += camSpeed;
+    if(event.key == 83)
+        camX -= camSpeed;
 }
+
 
 int main() {
     // C++ Version
@@ -54,102 +71,6 @@ int main() {
     LILA_WARN("Failed identify C++ compiler")
     #endif
 
-    /*
-     * Matrix Example
-     */
-
-     Lila::Matrix4f matrix = Lila::Matrix4f::identity();
-
-     LILA_DEBUG("{} {} {} {}", matrix.get(0,0), matrix.get(1,1), matrix.get(2,2), matrix.get(3,3));
-
-    /*
-     * Quaternion Example
-     */
-
-     Lila::Quaternionf q = Lila::Quaternionf::fromEuler(0.0f, 0.0f, 0.0f);
-     Lila::Vec3f euler = q.toEuler();
-     LILA_DEBUG("{} {} {}", euler.x, euler.y, euler.z);
-
-     f32 dot = q.dot(Lila::Quaternionf::identity());
-     LILA_DEBUG("Dot product: {}", dot);
-
-
-     Lila::Quaterniond q90 = Lila::Quaterniond::fromEuler(0.0f, 3.1415926535f / 2.0f, 0.0f);
-     LILA_DEBUG("{} {} {} {}", q90.w, q90.x, q90.y, q90.z);
-     Lila::Vec3d euler90 = q90.toEuler();
-     LILA_DEBUG("{} {} {}", euler90.x, euler90.y, euler90.z);
-
-     f64 dot90 = q90.dot(Lila::Quaterniond::identity());
-     LILA_DEBUG("Dot product: {}", dot90);
-
-
-     // Multiplication oders
-     Lila::Quaternionf q1 = Lila::Quaternionf(1, 1, 0, 0);
-     Lila::Quaternionf q2 = Lila::Quaternionf(1, 0, 1, 1);
-
-     Lila::Quaternionf a = q1 * q2;
-     Lila::Quaternionf b = q2 * q1;
-
-     LILA_DEBUG("q1 * q2 = {} {} {} {}", a.w, a.x, a.y, a.z);
-     LILA_DEBUG("q2 * q1 = {} {} {} {}", b.w, b.x, b.y, b.z);
-
-     b8 check = a != b;
-     LILA_DEBUG("Quaternion multiplication order check: {}", check);
-
-     LILA_DEBUG("Quaternion norm / magnitude check: {}", a.norm());
-
-     Lila::Quaternionf p = Lila::Quaternionf(0.333f, 0.666f, 0, 0);
-     Lila::Quaternionf pq = p.inverse();
-
-     Lila::Quaternionf c = p * pq;
-     Lila::Quaternionf d = pq * p;
-
-     LILA_DEBUG("p * p^-1 = {} {} {} {}", c.w, c.x, c.y, c.z);
-     LILA_DEBUG("p^-1 * p = {} {} {} {}", d.w, d.x, d.y, d.z);
-
-    /*
-     * ECS Example
-     */
-    Lila::ECS::EntityManager em;
-    Lila::ECS::ComponentManager cm;
-
-    cm.registerComponent<Transform>();
-    cm.registerComponent<Tag>();
-
-    Lila::ECS::Entity player = em.createEntity();
-    Lila::ECS::Entity enemy = em.createEntity();
-
-    cm.addComponent(player, Transform{0.0f, 0.0f, 0.0f});
-    cm.addComponent(player, Tag{5734});
-
-    cm.addComponent(enemy, Transform{5.0f, 0.0f, 0.0f});
-
-    Lila::ECS::ComponentSignature sigPlayer;
-    sigPlayer.set(0);
-    sigPlayer.set(1);
-    em.setSignature(player, sigPlayer);
-
-    Lila::ECS::ComponentSignature sigEnemy;
-    sigEnemy.set(0);
-    em.setSignature(enemy, sigEnemy);
-
-    auto systemFunction = [&cm, &em]() {
-        for(Lila::ECS::Entity e : em.getAliveEntities()) {
-            auto sig = em.getSignature(e);
-            if(sig.test(0)) {
-                auto& t = cm.getComponent<Transform>(e);
-
-                LILA_DEBUG("{}, {}, {}", t.x, t.y, t.z)
-            }
-            if(sig.test(1)) {
-                auto& tag = cm.getComponent<Tag>(e);
-                LILA_DEBUG("{}", tag.tag)
-            }
-        }
-    };
-
-    systemFunction();
-
     Lila::WindowSpecs windowSpecs;
 
     Lila::Window window = Lila::Window(windowSpecs);
@@ -167,25 +88,66 @@ int main() {
     Lila::setCallback(window, &bus);
 
     std::vector<f32> vertices {
-         1,  1, -1,   1, 1,
-         1, -1, -1,   1, 0,
-        -1, -1, -1,   0, 0,
-        -1,  1, -1,   0, 1
+        -1, -1,  1,
+         1, -1,  1,
+        -1,  1,  1,
+         1,  1,  1,
+        -1, -1, -1,
+         1, -1, -1,
+        -1,  1, -1,
+         1,  1, -1
     };
     std::vector<u32> indices {
+        2, 6, 7,
+        2, 3, 7,
+        0, 4, 5,
+        0, 1, 5,
+        0, 2, 6,
+        0, 4, 6,
+        1, 3, 7,
+        1, 5, 7,
+        0, 2, 3,
         0, 1, 3,
-        1, 2, 3
+        4, 6, 7,
+        4, 5, 7
     };
 
     Unique<OpenGL::GLGeometry> geometry = unique<OpenGL::GLGeometry>(vertices, indices);
     Unique<OpenGL::GLShader> shader = unique<OpenGL::GLShader>(Lila::getAssetPath() / "default.vert", Lila::getAssetPath() / "default.frag");
+
+    /*
+     * ECS Example
+     */
+    Lila::ECS::EntityManager em;
+    Lila::ECS::ComponentManager cm;
+
+    cm.registerComponent<Lila::CameraComponent>();
+
+    Lila::ECS::Entity camera = em.createEntity();
+
+    cm.addComponent(camera, Lila::CameraComponent{});
+
+    glm::mat4 view = glm::lookAt(glm::vec3(-4.0f, 3.0f, -2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
     glEnable(GL_DEPTH_TEST);
     while(!glfwWindowShouldClose(window.getHandle())) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClearColor(0.9f, 0.5f, 0.81f, 1.0f);
 
-        shader->bind();
+		view = glm::lookAt(glm::vec3(camX, camY, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+		shader->bind();
+
+        shader->uniformMatrix(
+            "projection",
+            Lila::getProjection(cm.getComponent<Lila::CameraComponent>(camera))
+        );
+
+        shader->uniformMatrix(
+            "view",
+            view
+        );
+
         geometry->render();
         shader->unbind();
 
