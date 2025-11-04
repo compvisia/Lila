47b821142369f69f7254c141060cea4705147f96
Youri <yourischijff@gmail.com>
2025-11-04 15:06:19 +0100

    Change Logger
    Assert now has a trap.
    Logging now more flexable

--- Lila/src/Log/Macros.h
+++ Lila/src/Log/Macros.h
@@ -4,17 +4,24 @@
 
 namespace Lila {
 
-    #define LILA_FATAL(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::FATAL, "Lila", fmt, ##__VA_ARGS__)
-    #define LILA_ERROR(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::ERROR, "Lila", fmt, ##__VA_ARGS__);
-    #define LILA_WARN(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::WARN, "Lila", fmt, ##__VA_ARGS__);
-    #define LILA_INFO(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::INFO, "Lila", fmt, ##__VA_ARGS__);
-    #define LILA_DEBUG(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::DEBUG, "Lila", fmt, ##__VA_ARGS__);
-    #define LILA_TRACE(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::TRACE, "Lila", fmt, ##__VA_ARGS__);
+    #define LILA_FATAL(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::FATAL, "(Lila)", fmt, ##__VA_ARGS__)
+    #define LILA_ERROR(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::ERROR, "(Lila)", fmt, ##__VA_ARGS__);
+    #define LILA_WARN(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::WARN, "(Lila)", fmt, ##__VA_ARGS__);
+    #define LILA_INFO(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::INFO, "(Lila)", fmt, ##__VA_ARGS__);
+    #define LILA_DEBUG(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::DEBUG, "(Lila)", fmt, ##__VA_ARGS__);
+    #define LILA_TRACE(fmt, ...) Lila::Logger::get().log(Lila::LogLevel::TRACE, "(Lila)", fmt, ##__VA_ARGS__);
 
-    // TODO: Implement break
+    #ifdef LILA_PLATFORM_WINDOWS
+    #define LILA_TRAP() __debugbreak();
+    #else
+    #define LILA_TRAP() __builtin_trap();
+    #endif
+
     #define LILA_ASSERT(expr, fmt, ...) { \
         if(!(expr)) { \
             Lila::Logger::get().log(Lila::LogLevel::ASSERT, #expr, fmt, ##__VA_ARGS__); \
+            Lila::Logger::get().log(Lila::LogLevel::ERROR, "Assertion at:", "{}:{} {}(...)", __FILE__, __LINE__, __func__); \
+            LILA_TRAP(); \
         } \
     }
 } // namespace Lila
