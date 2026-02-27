54827dec9011554d582b6550a2cf1a080a7af447
Youri <yourischijff@gmail.com>
2026-02-27 16:20:28 +0100

    Convert App to Dynamic Library

--- App/src/App.cpp
+++ App/src/App.cpp
@@ -39,12 +39,12 @@ struct Tag {
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f,  0.0f);
-f32 cameraSpeed = 0.05;
+f32 cameraSpeed = 0.05f;
 
 void keyEventFunction(Lila::KeyEvent event) {
-    if(event.key == 65) // A
+    if (event.key == 65) // A
         cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
-    if(event.key == 68) // D
+    if (event.key == 68) // D
         cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 
     // if(event.key == 340) // Shift
@@ -52,9 +52,9 @@ void keyEventFunction(Lila::KeyEvent event) {
     // if(event.key == 32) // Space
     //     camY += camSpeed;
 
-    if(event.key == 87) // W
+    if (event.key == 87) // W
         cameraPos += cameraSpeed * cameraFront;
-    if(event.key == 83) // S
+    if (event.key == 83) // S
         cameraPos -= cameraSpeed * cameraFront;
 }
 
@@ -75,17 +75,17 @@ void mousePositionEventFunction(Lila::MousePositionEvent event) {
     yaw += offsetX;
     pitch += offsetY;
 
-    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    direction.y = sin(glm::radians(pitch));
-    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.x = static_cast<f32>(cos(glm::radians(yaw)) * cos(glm::radians(pitch)));
+    direction.y = static_cast<f32>(sin(glm::radians(pitch)));
+    direction.z = static_cast<f32>(sin(glm::radians(yaw)) * cos(glm::radians(pitch)));
 
     cameraFront = glm::normalize(direction);
 }
 
-int main() {
+const Lila::Application& GameEntry() {
     Lila::RenderProfile profile;
     profile.renderApi = Lila::RenderApi::OpenGL;
-    Lila::Application app(profile);
+    static Lila::Application app = Lila::Application(profile);
 
     auto& window = app.getWindow();
 
@@ -162,17 +162,21 @@ int main() {
 
     Lila::Testing::registerForRendering(app, entity);
 
-    glEnable(GL_DEPTH_TEST);
-    while(window.isRunning()) {
-        window.update();
+  //   glEnable(GL_DEPTH_TEST);
+  //   while (window.isRunning()) {
+  //       window.update();
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0.9f, 0.5f, 0.81f, 1.0f);
+  //       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// glClearColor(0.9f, 0.5f, 0.81f, 1.0f);
 
-		Lila::Testing::render(app);
-    }
+		// Lila::Testing::render(app);
+  //   }
 
     Lila::Testing::destroy();
 
-    LILA_INFO("Exiting...");
+    return app;
+}
+
+extern "C" LILA_API const Lila::Application& CreateApplication() {
+    return GameEntry();
 }
