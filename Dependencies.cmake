7e128812692755d2155229274b16c949af179754
Youri <youri@mailfence.com>
2026-04-07 13:37:29 +0200

    Remove whitespace CMake files

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -6,19 +6,19 @@ include(FetchContent)
 find_package(glfw3 3.4 QUIET)
 
 if (NOT glfw3_FOUND)
-    set(GLFW_BUILD_WAYLAND OFF)
-    FetchContent_Declare(
+  set(GLFW_BUILD_WAYLAND OFF)
+  FetchContent_Declare(
             glfw3
             DOWNLOAD_EXTRACT_TIMESTAMP OFF
             URL https://github.com/compvisia/glfw/archive/refs/heads/master.zip
     )
 
-    FetchContent_GetProperties(glfw3)
+  FetchContent_GetProperties(glfw3)
 
-    if (NOT glfw3_POPULATED)
-        set(FETCHCONTENT_QUIET NO)
-        FetchContent_MakeAvailable(glfw3)
-    endif()
+  if (NOT glfw3_POPULATED)
+    set(FETCHCONTENT_QUIET NO)
+    FetchContent_MakeAvailable(glfw3)
+  endif()
 endif()
 
 # OpenGL / GLAD
@@ -33,8 +33,8 @@ FetchContent_Declare(
 FetchContent_GetProperties(glad)
 
 if(NOT glad_POPULATED)
-    set(FETCHCONTENT_QUIET NO)
-    FetchContent_MakeAvailable(glad)
+  set(FETCHCONTENT_QUIET NO)
+  FetchContent_MakeAvailable(glad)
 endif()
 
 set_target_properties(glad PROPERTIES FOLDER "Dependencies")
