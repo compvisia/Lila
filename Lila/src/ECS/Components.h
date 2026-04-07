d4f583457af121397c5828ff28ac5f966fa57f42
Youri <youri@mailfence.com>
2026-04-07 13:40:56 +0200

    Fix missing struct declaration

--- Lila/src/ECS/Components.h
+++ Lila/src/ECS/Components.h
@@ -2,9 +2,24 @@
 
 #include <bitset>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+#include "Common/UUID.h"
+
 namespace Lila::ECS {
 
     constexpr size_t MAX_COMPONENTS = 64;
     using ComponentSignature = std::bitset<MAX_COMPONENTS>;
+
+    struct Tag {
+        Lila::UUID uuid;
+    };
+
+    struct Transform {
+        glm::vec3 position;
+        glm::quat rotation;
+        glm::vec3 scalar;
+    };
 
 } // namespace Lila::ECS
